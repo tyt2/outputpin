abc4c933d415243d2584706784e624a9a2a25d5f
tyt2 <sachingm@gmail.com>
2020-10-19 16:15:02 -0400

    Simple On off

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+})
+input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
 })
 basic.forever(function () {
