afaeba01b8c85e97e83d2f8b5023a2c3c2fa4466
tyt2 <sachingm@gmail.com>
2020-10-19 17:51:49 -0400

    Improved Toggle logic & 3 pin support

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    togglePin1()
+    togglePin(DigitalPin.P0)
 })
-function togglePin1 () {
-    Pin1Value = Math.abs(Pin1Value - 1)
-    pins.digitalWritePin(DigitalPin.P0, Pin1Value)
-}
-function togglePin2 () {
-    Pin2Value = Math.abs(Pin2Value - 1)
-    pins.digitalWritePin(DigitalPin.P1, Pin2Value)
-}
 input.onButtonPressed(Button.B, function () {
-    togglePin2()
+    togglePin(DigitalPin.P1)
 })
-let Pin2Value = 0
-let Pin1Value = 0
-Pin1Value = 0
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    togglePin(DigitalPin.P2)
 })
+function togglePin (thePin: DigitalPin) {
+    let pinValue = 0
+    pinValue = pins.digitalReadPin(thePin) //DigitalPin.P0)
+    pinValue = Math.abs(pinValue - 1)
+    pins.digitalWritePin(thePin, pinValue)
+    if (pinValue == 0) {
+        led.unplot(thePin-100, 0)
+    } else {
+        led.plot(thePin-100, 0)
+    }
+}
