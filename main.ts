3d31761027b00fdb98408af3f780c798053fdbe3
tyt2 <sachingm@gmail.com>
2020-10-19 16:26:59 -0400

    Toggle Pins1 & 2

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    togglePin1()
 })
+function togglePin1 () {
+    Pin1Value = Math.abs(Pin1Value - 1)
+    pins.digitalWritePin(DigitalPin.P0, Pin1Value)
+}
+function togglePin2 () {
+    Pin2Value = Math.abs(Pin2Value - 1)
+    pins.digitalWritePin(DigitalPin.P1, Pin2Value)
+}
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    togglePin2()
 })
+let Pin2Value = 0
+let Pin1Value = 0
+Pin1Value = 0
 basic.forever(function () {
 	
 })
