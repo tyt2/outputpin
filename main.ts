cb4c84d83ad5fd8f4a547348a1ee6e4ed399f719
tyt2 <sachingm@gmail.com>
2020-10-20 13:54:54 -0400

    Added function to fluctuate pin. Added flag to detect button press and break from loop.

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     togglePin(DigitalPin.P0)
+    buttonPressed = true
 })
 input.onButtonPressed(Button.B, function () {
-    togglePin(DigitalPin.P1)
+    pinFluctuate(AnalogPin.P0)
 })
 input.onGesture(Gesture.Shake, function () {
-    togglePin(DigitalPin.P2)
+    //pins.analogWritePin(AnalogPin.P0, 0)
+    togglePin(DigitalPin.P1)
 })
 function togglePin (thePin: DigitalPin) { 
     // get pin number
@@ -24,10 +26,28 @@ function togglePin (thePin: DigitalPin) {
         led.plot(thePinNumber, 0)
     }
 }
+function pinFluctuate(thePin: AnalogPin) {
+    let start = 50
+    let max = 100
+    led.unplot(thePin - 100, 4)
+    for (let index = 0; index <= max; index++) {
+        pins.analogWritePin(thePin, 1023 * Math.constrain((start + index * (100 - start) / max) / 100, 0, 1))
+        if (buttonPressed) {
+            buttonPressed = false
+            break
+        }
+        basic.pause(30)
+    }
+    led.plot(thePin - 100, 4)
+}
+function resetAllPins() {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+}
 // Initialize
-let pinValues = [0, 0, 0]
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P2, 0)
 basic.showIcon(IconNames.Yes)
+let pinValues = [0, 0, 0]
+let buttonPressed = false
+resetAllPins()
 basic.clearScreen()
